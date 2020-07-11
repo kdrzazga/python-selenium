9f8261f24a7c9f3fc157bc51e9530317a3ced560
KDRZAZGA <krzysztof.drzazga@op.pl>
2020-07-11 17:20:55 +0200

    Python Tutorial initial commit

--- ryan-air-selenium-tests/Page.py
+++ ryan-air-selenium-tests/Page.py
@@ -1,10 +1,5 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import time
 from PageBase import PageBase
 
 class MainPage(PageBase):
